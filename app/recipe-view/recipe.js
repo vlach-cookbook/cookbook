270568fca911e222c6edfb619db1eb5d5ab3063e
Jeffrey Yasskin <jyasskin@gmail.com>
2015-04-12 18:03:59 -0700

    Make adding and removing ingredients work, and switch between edit and view modes.

--- app/recipe-view/recipe.js
+++ app/recipe-view/recipe.js
@@ -27,28 +27,49 @@ angular.module('cookbookApp.recipe', ['ngRoute'])
   });
 }])
 
-.controller('RecipeCtrl', ['$scope', '$routeParams', '$firebaseObject', 'FBURL', '$location',
-  function($scope, $routeParams, $firebaseObject, FBURL, $location) {
+.controller('RecipeCtrl', ['$scope', '$routeParams', '$firebaseObject', '$firebaseArray', 'FBURL', '$location',
+  function($scope, $routeParams, $firebaseObject, $firebaseArray, FBURL, $location) {
     var recipes = new Firebase(FBURL).child('recipes');
     var titles = new Firebase(FBURL).child('titles');
-    if ($location.path() === '/new') {
-      $firebaseObject(recipes.push()).$bindTo($scope, 'recipe');
-      $scope.edit = true;
-    } else {
-      $scope.spinner = true;
-      $scope.edit = $location.search()['edit'] !== undefined;
-      new Firebase(FBURL).child('titles').child($routeParams.recipeTitle).once('value', function(snapshot) {
-        var recipeId = snapshot.val();
-        $scope.spinner = false;
-        $firebaseObject(recipes.child(recipeId)).$bindTo($scope, 'recipe');
-      });
-    }
+    Promise.resolve().then(function() {
+      if ($location.path() === '/new') {
+        $scope.editing = true;
+        return recipes.push({title: '', ingredients: {}, directions: ''});
+      } else {
+        $scope.spinner = true;
+        $scope.editing = $location.search()['edit'] !== undefined;
+        return new Promise(function(resolve, reject) {
+          titles.child($routeParams.recipeTitle)
+            .once('value', function(snapshot) {
+              resolve(recipes.child(snapshot.val()));
+            });
+        });
+      }
+    }).then(function(recipePath) {
+      $scope.recipe_ingredients = recipePath.child('ingredients');
+      return $firebaseObject(recipePath).$bindTo($scope, 'recipe');
+    }).then(function() {
+      $scope.spinner = false;
+    });
+
     function sanitizeTitle(title) {
       return title.toLowerCase().replace(/[^a-z0-9]+/g, '-');
     }
     $scope.setTitle = function() {
-      console.log('Setting title "' + $scope.recipe.title + '" to ' + $scope.recipe.$id);
       titles.child(sanitizeTitle($scope.recipe.title)).set($scope.recipe.$id);
+    };
+    $scope.addIngredient = function() {
+      $scope.recipe_ingredients.push({
+        count: '', unit: '', name: '', preparation: ''});
+    };
+    $scope.removeIngredient = function(key) {
+      $scope.recipe_ingredients.child(key).remove();
+    };
+    $scope.save = function() {
+      $location.path('/recipe/' + sanitizeTitle($scope.recipe.title)).search('');
+    };
+    $scope.edit = function() {
+      $location.search({'edit': 1});
     };
 }])
 ;
