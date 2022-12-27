eff5f743f603592d7e38dfdb170ba4dc64c1cd21
Jeffrey Yasskin <jyasskin@gmail.com>
2022-12-27 12:03:26 -0800

    Save the initial schema migration.

---
+++ webserver/prisma/migrations/20221227195700_init/migration.sql
@@ -0,0 +1,112 @@
+-- CreateExtension
+CREATE EXTENSION IF NOT EXISTS "citext";
+
+-- CreateTable
+CREATE TABLE "Recipe" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMPTZ(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMPTZ(3) NOT NULL,
+    "authorId" UUID NOT NULL,
+    "name" CITEXT NOT NULL,
+    "slug" CITEXT NOT NULL,
+    "servings" INTEGER,
+    "steps" TEXT[],
+
+    CONSTRAINT "Recipe_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "RecipeIngredient" (
+    "id" SERIAL NOT NULL,
+    "order" INTEGER,
+    "amount" TEXT,
+    "unit" TEXT,
+    "name" TEXT NOT NULL,
+    "preparation" TEXT,
+    "recipeId" INTEGER NOT NULL,
+
+    CONSTRAINT "RecipeIngredient_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" SERIAL NOT NULL,
+    "name" CITEXT NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" UUID NOT NULL,
+    "name" TEXT,
+    "username" CITEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "GoogleUser" (
+    "gid" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "userId" UUID NOT NULL,
+
+    CONSTRAINT "GoogleUser_pkey" PRIMARY KEY ("gid")
+);
+
+-- CreateTable
+CREATE TABLE "Session" (
+    "id" TEXT NOT NULL,
+    "userId" UUID NOT NULL,
+    "expires" TIMESTAMPTZ(3) NOT NULL,
+
+    CONSTRAINT "Session_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_CategoryToRecipe" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Recipe_authorId_name_key" ON "Recipe"("authorId", "name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Recipe_authorId_slug_key" ON "Recipe"("authorId", "slug");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "RecipeIngredient_recipeId_order_key" ON "RecipeIngredient"("recipeId", "order");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Category_name_key" ON "Category"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "GoogleUser_email_key" ON "GoogleUser"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_CategoryToRecipe_AB_unique" ON "_CategoryToRecipe"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_CategoryToRecipe_B_index" ON "_CategoryToRecipe"("B");
+
+-- AddForeignKey
+ALTER TABLE "Recipe" ADD CONSTRAINT "Recipe_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "RecipeIngredient" ADD CONSTRAINT "RecipeIngredient_recipeId_fkey" FOREIGN KEY ("recipeId") REFERENCES "Recipe"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "GoogleUser" ADD CONSTRAINT "GoogleUser_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Session" ADD CONSTRAINT "Session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_CategoryToRecipe" ADD CONSTRAINT "_CategoryToRecipe_A_fkey" FOREIGN KEY ("A") REFERENCES "Category"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_CategoryToRecipe" ADD CONSTRAINT "_CategoryToRecipe_B_fkey" FOREIGN KEY ("B") REFERENCES "Recipe"("id") ON DELETE CASCADE ON UPDATE CASCADE;
