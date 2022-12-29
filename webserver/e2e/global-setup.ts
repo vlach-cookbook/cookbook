fe41515418a9e161eb2a1677b525e0ce3d0700bd
Jeffrey Yasskin <jyasskin@gmail.com>
2022-12-29 13:32:06 -0800

    Optimize the CI build, and show the webserver's startup output.

--- webserver/e2e/global-setup.ts
+++ webserver/e2e/global-setup.ts
@@ -5,6 +5,10 @@ import { promisify } from 'node:util';
 const execFileP = promisify(execFile);
 
 async function globalSetup(config: FullConfig) {
+  if (process.env.CI) {
+    // The CI has already reset the database by the time we get here.
+    return;
+  }
   // Reset the database so that tests work even if we're re-using a webserver instance.
   const { stdout, stderr } = await execFileP('pnpm',
     ['prisma', 'db', 'push', '--skip-generate', '--force-reset']);
