ad27698dd5a53e03351570e746cc31309d609fae
Jeffrey Yasskin <jyasskin@gmail.com>
2022-12-31 14:55:15 -0800

    Database users are now managed by Terraform.

--- db/create.sh
+++ db/create.sh
@@ -3,9 +3,4 @@
 . .env
 
 flyctl postgres create --name vlach-cookbook-db --region sea --initial-cluster-size 2 --vm-size shared-cpu-1x --volume-size 1
-flyctl postgres connect <<EOF
-CREATE ROLE cookbook_admin LOGIN PASSWORD '$COOKBOOK_ADMIN_PASSWORD';
-CREATE DATABASE cookbook OWNER cookbook_admin;
-CREATE DATABASE cookbook_shadow OWNER cookbook_admin;
-quit;
-EOF
+terraform apply
