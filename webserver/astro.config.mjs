61e3704e2c5cb81aeebe6b1a15c7e3128849949a
Jeffrey Yasskin <jyasskin@gmail.com>
2023-01-27 21:10:30 -0800

    Use Fly.io machines to auto-stop the staging server when it's not in use.
    * Teach the server to exit after $MAX_IDLE_SECONDS of no active requests.
    * Update the machines image within the github action.

--- webserver/astro.config.mjs
+++ webserver/astro.config.mjs
@@ -8,7 +8,7 @@ import solidJs from "@astrojs/solid-js";
 export default defineConfig({
   output: 'server',
   adapter: node({
-    mode: 'standalone'
+    mode: 'middleware'
   }),
   integrations: [solidJs()]
 });
