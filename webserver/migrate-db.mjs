24353d50ddb6869431a8d8d5286c8d98ae4184a8
Jeffrey Yasskin <jyasskin@gmail.com>
2024-01-15 02:07:31 +0000

    Use backups to populate the staging database.
    This continuously tests restores and lets me remove prod access from the
    staging server.

--- webserver/migrate-db.mjs
+++ webserver/migrate-db.mjs
@@ -1,12 +1,33 @@
+import { Storage } from '@google-cloud/storage';
+import age from "age-encryption";
 import { execSync } from 'node:child_process';
+import zlib from 'node:zlib';
 
 if (process.env.FLY_APP_NAME === 'vlach-cookbook-staging') {
-  // Copy the prod database over the staging one before running the Prisma
+  // Copy the latest backup over the staging database before running the Prisma
   // migration, to make sure the migration will work when the app is released.
-  const PROD_DATABASE_URL = process.env.PROD_WEBSERVER_DATABASE_URL
   const STAGING_DATABASE_URL = process.env.ADMIN_DATABASE_URL
 
-  const dump = execSync(`pg_dump --clean --if-exists -O -d "${PROD_DATABASE_URL}"`, { encoding: 'utf-8' });
+  const storage = new Storage({
+    projectId: 'vlach-cookbook',
+    credentials: JSON.parse(process.env.MIGRATION_GOOGLE_SERVICE_ACCOUNT_CREDENTIALS)
+  });
+
+  const bucket = storage.bucket('vlach-cookbook-backup');
+  // Look for backups starting a month ago.
+  let earliestBackupTime = new Date(Date.now() - 30 * 24 * 60 * 60 * 1000);
+  const [recentBackups] = await bucket.getFiles({ startOffset: `backup-${earliestBackupTime.toISOString()}` });
+  if (recentBackups.length === 0) {
+    throw new Error(`Couldn't find a backup since ${earliestBackupTime}.`);
+  }
+  const [contents] = await recentBackups[recentBackups.length - 1].download();
+
+  const { Decrypter } = await age();
+  const d = new Decrypter();
+  d.addIdentity(process.env.MIGRATION_AGE_BACKUP_IDENTITY);
+  const decrypted = d.decrypt(contents);
+
+  const dump = zlib.brotliDecompressSync(decrypted).toString('utf-8');
 
   // Clear the whole database without actually deleting it. Otherwise, when we
   // add new tables that aren't in Prod yet, the *second* deployment to Staging
