47a4e2f97d59cdfdcdb4351f6aadd194c941bfbd
Jeffrey Yasskin <jyasskin@gmail.com>
2024-01-23 06:23:56 +0000

    Enable Sentry error logging.

--- webserver/astro.config.mjs
+++ webserver/astro.config.mjs
@@ -1,8 +1,7 @@
 import node from "@astrojs/node";
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
 import solidJs from "@astrojs/solid-js";
+import sentry from "@sentry/astro";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,5 +9,13 @@ export default defineConfig({
   adapter: node({
     mode: 'standalone'
   }),
-  integrations: [solidJs()]
+  integrations: [solidJs(), sentry({
+    dsn: process.env.SENTRY_DSN,
+    release: process.env.SENTRY_RELEASE,
+    environment: process.env.SENTRY_ENVIRONMENT ?? "development",
+    sourceMapsUploadOptions: {
+      project: "vlach-cookbook",
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+    },
+  })]
 });
