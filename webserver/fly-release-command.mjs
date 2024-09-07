7f6decd0d0117a7b1ecf81330822c4f38273fe34
Jeffrey Yasskin <jyasskin@gmail.com>
2024-09-07 03:59:06 +0000

    Turn on swap during deployment to avoid out-of-memory in `prisma migrate deploy`.
    Thanks to https://community.fly.io/t/prisma-sqlite-causes-an-out-of-memory-error-on-deploy/11039
    for the instructions.

--- webserver/fly-release-command.mjs
+++ webserver/fly-release-command.mjs
@@ -1,6 +1,13 @@
 import { execSync } from 'node:child_process';
 import { replaceDbWithBackup } from './replaceDbWithBackup.mjs';
 
+// Turn on swap to avoid out-of-memory errors during `prisma migrate deploy`
+console.log(execSync('fallocate -l 256M /swapfile', {encoding: 'utf-8'}));
+console.log(execSync('chmod 0600 /swapfile', {encoding: 'utf-8'}));
+console.log(execSync('mkswap /swapfile', {encoding: 'utf-8'}));
+console.log(execSync('echo 10 > /proc/sys/vm/swappiness', {encoding: 'utf-8'}));
+console.log(execSync('swapon /swapfile', {encoding: 'utf-8'}));
+
 if (process.env.FLY_APP_NAME === 'vlach-cookbook-staging') {
   // Copy the latest backup over the staging database before running the Prisma
   // migration, to make sure the migration will work when the app is released.
